eaf4a68864f872076d01d7b54425abdc35f3f30c
kiraxkir <victoirembiya83@gmail.com>
2025-06-01 18:39:27 +0100

    finalisation de la boucle pour convertir les int en str pour en suite les stocker dans le fichier resulatat

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 from pprint import pprint
 import round_cle
+from copy import copy
 
 def encryption (message,user_cle):
     message=[c for c in message]
@@ -45,24 +46,24 @@ def decryptage(message,user_cle):
 texte=plaintext.plaintext("exemple.txt")
 cle=[[[88, 107, 119, 110], [111, 112, 97, 126], [100, 75, 65, 105], [102, 82, 112, 66]], [[92, 111, 115, 98], [107, 116, 101, 114], [96, 79, 69, 101], [98, 86, 116, 78]], [[80, 99, 127, 102], [103, 120, 105, 118], [108, 67, 73, 97], [110, 90, 120, 74]], [[84, 103, 123, 122], [99, 124, 109, 106], [104, 71, 77, 125], [106, 94, 124, 86]], [[72, 123, 103, 126], [127, 96, 113, 110], [116, 91, 81, 121], [118, 66, 96, 82]]]
 
-resultat=[]
-for i in texte :
-    for j in range (5):
-        i=encryption(i,cle[j])
-    resultat.append(i)
 
-print(resultat)
-print("\n")
-print("\n")
-print("\n")
-print("-------------------------------------------------------decrypatge------------------------------------------------------------")
+crypte= []
+for i in texte : 
+    x=i
+    for j in range ( 5): 
+        x=encryption(x,cle[j])
+    for index in range(4):
+        for h in range( 4):
+            x[index][h] = chr(x[index][h])
+    crypte.append(''.join([char for ligne in x for char in ligne]))
 
-resultat2=[]
-for i in resultat : 
-    for j in range (1,5) :
-        i=decryptage(i,cle[-j])
-    resultat2.append(i)
+    
+# print(crypte)
+# decrype=[]
+# for i in crypte:
+#     y=i
+#     for j in range(1,6):
+#         y=decryptage(y,cle[-j])
+#     decrype.append(y)
 
-print(resultat2)
-print("-"*40)
-print(texte)
+
