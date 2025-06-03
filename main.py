36c18a73a2b8b48d38b7f96680d1fa55eda0fc8a
kiraxkir <victoirembiya83@gmail.com>
2025-06-03 18:11:27 +0100

    dans main, j ai ajouter la boucle cryptage qui permet d ecrire le resultat sur un fichier binaire et que je pourarais ouvrir dans plaintext

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 from pprint import pprint
 import round_cle
 from copy import copy
+import json
 
 def encryption (message,user_cle):
     message=[c for c in message]
@@ -39,31 +40,45 @@ def decryptage(message,user_cle):
     m4=subyte.invsubyte(m3,cle)
     return m4
 
+def cryptage(texte,cle):
+    resultat=[]
+    for i in texte : 
+        x=i
+        for j in range ( 5): 
+            x=encryption(x,cle[j])
+        resultat.append(x)
+   
 
+    with open("resultat.bin","wb") as f:
+        for block in resultat:
+            for ligne in block:
+                f.write(bytes(ligne))
 
 # start = time.perf_counter()
 
 texte=plaintext.plaintext("exemple.txt")
-cle=[[[88, 107, 119, 110], [111, 112, 97, 126], [100, 75, 65, 105], [102, 82, 112, 66]], [[92, 111, 115, 98], [107, 116, 101, 114], [96, 79, 69, 101], [98, 86, 116, 78]], [[80, 99, 127, 102], [103, 120, 105, 118], [108, 67, 73, 97], [110, 90, 120, 74]], [[84, 103, 123, 122], [99, 124, 109, 106], [104, 71, 77, 125], [106, 94, 124, 86]], [[72, 123, 103, 126], [127, 96, 113, 110], [116, 91, 81, 121], [118, 66, 96, 82]]]
+cle=round_cle.cle()
+print(texte)
 
 
-crypte= []
+resultat=[]
 for i in texte : 
-    x=i
-    for j in range ( 5): 
-        x=encryption(x,cle[j])
-    for index in range(4):
-        for h in range( 4):
-            x[index][h] = chr(x[index][h])
-    crypte.append(''.join([char for ligne in x for char in ligne]))
+        x=i
+        for j in range (5): 
+            x=encryption(x,cle[j])
+        resultat.append(x)
+print(resultat)
 
-    
-# print(crypte)
-# decrype=[]
-# for i in crypte:
-#     y=i
-#     for j in range(1,6):
-#         y=decryptage(y,cle[-j])
-#     decrype.append(y)
+decrype=[]
 
+for i in resultat:
+    y=i
+    for j in range(1,6):
+        y=decryptage(y,cle[-j])
+    for a in range(4):
+        for b in range(4):
+            x=chr(y[a][b])
+            decrype.append(x)       
+
+print(''.join([c for c in decrype]))     
 
